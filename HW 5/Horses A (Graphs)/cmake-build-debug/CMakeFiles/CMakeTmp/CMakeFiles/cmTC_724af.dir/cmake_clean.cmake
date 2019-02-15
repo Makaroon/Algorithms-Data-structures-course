47b98574213cb58e212990dd4788d6131daa1697
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-02-16 02:19:36 +0300

    Initial commit

---
+++ HW 5/Horses A (Graphs)/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_724af.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_724af.dir/feature_tests.c.obj"
+  "cmTC_724af.pdb"
+  "cmTC_724af.exe"
+  "cmTC_724af.exe.manifest"
+  "libcmTC_724af.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_724af.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

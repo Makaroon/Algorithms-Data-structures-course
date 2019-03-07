3cdb1d9b1567fe5d24a557b1294a9442408b7865
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-03-07 16:51:58 +0300

    kruskal algo

--- HW 7/Boruvka algorithm/Boruvka/main.cpp
+++ HW 7/Boruvka algorithm/Boruvka/main.cpp
@@ -4,18 +4,59 @@
 //Можно создавать любые классы и методы для решения задачи
 
 using namespace std;
+const Edge NULL_EDGE{-1, -1, 30001, -1};
 
-//Основной метод решения задачи, параметры:
-//N - количество вершин, M - количество ребер в графе
-//edges - вектор ребер, каждое ребро представлено 3-мя числами (А,В,W), где A и B - номера вершин, которые оно соединяет, и W - вес ребра,
-//передается по ссылке (&), чтобы не копировать, изменять вектор и его значения можно.
-//Результат также в виде вектора ребер, передается по ссылке (&), чтобы не копировать его.
+///Основной метод решения задачи, параметры:
+///N - количество вершин, M - количество ребер в графе
+///edges - вектор ребер, каждое ребро представлено 3-мя числами (А,В,W), где A и B - номера вершин, которые оно соединяет, и W - вес ребра,
+///передается по ссылке (&), чтобы не копировать, изменять вектор и его значения можно.
+///Результат также в виде вектора ребер, передается по ссылке (&), чтобы не копировать его.
 void solve(int N, int M, vector<Edge>& edges, vector<Edge>& result)
 {
     //Советую разделить решение на логические блоки
     //Можно использовать любые другие структуры, но затем скопировать ответ в структуру Edge для записи результата в файл.
     //Также можно добавить любые необходимые компараторы для предложенного класса Edge, так как все методы и поля публичные.
 
+    int n = N;
+    vector<int> component;
+    vector<bool> used(M, false);
+    vector<Edge> chipedges(n);
+    for (int i = 0; i < N; ++i)
+        component.emplace_back(i);
+    while (n > 1)
+    {
+        fill(chipedges.begin(), chipedges.begin() + n, NULL_EDGE);
+        for (int i = 0; i < M; ++i)
+        {
+            if (!used[i] && component[edges[i].A] != component[edges[i].B])
+            {
+                if (edges[i].W < chipedges[component[edges[i].A]].W)
+                    chipedges[component[edges[i].A]] = edges[i];
+                if (edges[i].W < chipedges[component[edges[i].B]].W)
+                    chipedges[component[edges[i].B]] = edges[i];
+            }
+        }
+        int s = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            Edge e = chipedges[i];
+            if (!used[e.number] && e.W != NULL_EDGE.W)
+            {
+                ++s;
+                int ca = component[e.A], cb = component[e.B];
+                used[e.number] = true;
+                result.emplace_back(e);
+                for (int k = 0; k < N; ++k)
+                {
+                    if (component[k] == ca)
+                        component[k] = cb;
+                    if (component[k] > ca)
+                        --component[k];
+                }
+            }
+        }
+        n -= s;
+    }
 }
 
 int main()

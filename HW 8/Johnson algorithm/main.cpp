c493cbd66f57c0e1a6b0c441e96b49fc071fd2a5
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-03-31 14:28:15 +0300

    Johnson alg #2

--- HW 8/Johnson algorithm/main.cpp
+++ HW 8/Johnson algorithm/main.cpp
@@ -10,15 +10,65 @@ void ReadWriter::writeValues(std::vector<std::vector<int>> result)
     if (!fout.is_open())
         throw std::ios_base::failure("file not open");
 
-	//Здесь вывод на печать, например так:
-	//Предлагается построчно выводить, как требуется в ответе
-	
-    for (int i = 0; i < result.size() - 1; i++)
-        fout << result[i][0] << ' ' << result[i][1] << ' ' << result[i][2] << "\n";
+    for (int i = 0; i < result[0].size(); ++i)
+        for (int j = 0; j < result[0].size(); ++j)
+            if (i != j)
+                fout << i << " " << j << " " << result[i][j] << '\n';
+}
 
-    fout << result[result.size() - 1][0] << ' ' << result[result.size() - 1][1] << ' ' << result[result.size() - 1][2];
-	//Можно сделать иначе - в этом же методе, но используя данные параметры метода (vector<vector<int>>)
-	//Для этого метод и вынесен в этот файл
+void relax(vector<int>& d, vector<int>& p, int v, int u, int path)
+{
+    if (d[v] > d[u] + path)
+    {
+        d[v] = d[u] + path;
+        p[v] = u;
+    }
+}
+
+///check if we entered all vertices
+bool allUsed(vector<bool>& used)
+{
+    bool flag = true;
+    for (bool b : used)
+        if (!b)
+            flag = false;
+    return flag;
+}
+
+///finding unused vertex with minimal distance
+///returning index of the vertex
+int findMin(vector<int>& d, vector<bool>& used, int N)
+{
+    int min = 30001;
+    int num = -1;
+    for (int i = 0; i < N; ++i)
+        if (!used[i])
+            if (d[i] < min)
+            {
+                num = i;
+                min = d[i];
+            }
+    return num;
+}
+
+void algBF(int N, int M, vector<Edge>& edges, vector<int>& result)
+{
+    vector<int> d(N);
+    vector<int> p(N);
+    for (int i = 0; i < N; ++i)
+    {
+        d[i] = 30001;
+        p[i] = -1;
+    }
+    d[0] = 0;
+
+    //Bellman-Ford
+    for (int i = 0; i < N - 1; ++i)
+        for (int j = 0; j < M; ++j)
+            relax(d, p, edges[j].B, edges[j].A, edges[j].W);
+
+    for (int i = 1; i < N; ++i)
+        result.push_back(d[i]);
 }
 
 //Основной метод решения задачи, параметры:
@@ -28,7 +78,34 @@ void ReadWriter::writeValues(std::vector<std::vector<int>> result)
 //передается по ссылке (&), чтобы не копировать, изменять вектор и его значения можно.
 void solve(int N, int M, vector<Edge>& edges, vector<vector<int>>& result)
 {
-    //Советую разделить решение на логические блоки
+    ///initialization
+    vector<vector<int>> D(N, vector<int>(N));
+
+
+    for (int i = 0; i < N; ++i)
+    {
+        Edge e;
+        e.A=N;
+        e.B=i;
+        e.W=0;
+        e.number=edges.size();
+        edges.push_back(e);
+    }
+
+    vector<int> h;
+    ///..
+    algBF(N, static_cast<int>(edges.size()), edges, h);
+
+    for (int i = 0; i < edges.size(); ++i)
+        edges[i].W+=h[edges[i].A]-h[edges[i].B];
+
+    for(int i=0;i<N+1;++i)
+    {
+        vector<vector<int>> s;
+        ///..
+        for(int j=0;j<N+1;++j)
+            D[i][j]=s[i][j]+h[j]-h[i];
+    }
 
 }
 

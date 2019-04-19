cda86c4df556c5eee8946dbfb61d57431286284e
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-04-19 14:18:28 +0300

    Started HW 11

---
+++ HW 11/Wagner-Fischer Algorithm/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+//Необходимо реализовать метод
+//о алгоритме можно прочитать в источниках указанных в программе курса, или на странице курса в ЛМС или в презентациях к семинару.
+int Wagner_Fischer(string& s, string& t)
+{
+
+}
+
+//Не изменять метод main без крайней необходимости
+//ОБЯЗАТЕЛЬНО добавить в комментариях подробные пояснения и причины побудившие вас изменить код этого метода.
+int main(int argc, const char * argv[]) 
+{    
+    int n;
+    fstream fin;
+    fstream fout;
+    fin.open("input.txt",ios::in);
+    fout.open("output.txt",ios::out);
+    if(fin.is_open()) {
+        string N;
+        getline(fin,N);
+        n = atoi( N.c_str());
+        for (int i = 0; i < n; i++) {
+            string s;
+            string t;
+            getline(fin,s);
+            getline(fin,t);
+            fout << Wagner_Fischer(s, t) << (i == n-1 ? "" : " ");
+        }
+        fout.close();
+        fin.close();
+    }
+    return 0;
+}

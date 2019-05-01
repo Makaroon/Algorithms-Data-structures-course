a8197226b9fd7e06366b7a3c0a21807fa073f3ae
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-05-02 02:02:35 +0300

    solved task 1

--- HW 12/Hike (DP TASK 1)/main.cpp
+++ HW 12/Hike (DP TASK 1)/main.cpp
@@ -7,29 +7,52 @@ using namespace std;
 //Основная задача - реализовать данный метод
 int countMaxCross(string& riverMap)
 {
-	
+    int n = riverMap.length();
+    int left[n + 1];
+    int right[n + 1];
+
+    left[0] = 0;
+    right[0] = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+        int stepLeft = left[i - 1], stepRight = right[i - 1];
+
+        if (riverMap[i - 1] == 'L')
+            stepLeft++;
+        else if (riverMap[i - 1] == 'R')
+            stepRight++;
+        else if (riverMap[i - 1] == 'B')
+        {
+            stepLeft++;
+            stepRight++;
+        }
+        left[i] = std::min(stepLeft, stepRight + 1);
+        right[i] = std::min(stepRight, left[i] + 1);
+    }
+
+    return right[n];
 }
 
 
 int main()
 {
-	string riverMap;
-	int res;
-	
-	ifstream fin;
-	fin.open("input.txt");
-	if (fin.is_open())
-	{
-		getline(fin, riverMap);
-		fin.close();
-	}
-	
-	res = countMaxCross(riverMap);
-	
-	fstream fout;
-	fout.open("output.txt", ios::out);
-	fout << res;
-	fout.close();
-	
-	return 0;
+    string riverMap;
+    int res;
+
+    ifstream fin;
+    fin.open("input.txt");
+    if (fin.is_open())
+    {
+        getline(fin, riverMap);
+        fin.close();
+    }
+
+    res = countMaxCross(riverMap);
+
+    fstream fout;
+    fout.open("output.txt", ios::out);
+    fout << res;
+    fout.close();
+
+    return 0;
 }

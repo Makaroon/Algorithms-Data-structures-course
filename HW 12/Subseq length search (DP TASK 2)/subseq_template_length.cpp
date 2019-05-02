30356e39953c1658eacf0ff40c02afd1c4e5bd68
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-05-02 11:44:20 +0300

    tasks 2 & 3 rdy, small fixes for other

--- HW 12/Subseq length search (DP TASK 2)/subseq_template_length.cpp
+++ HW 12/Subseq length search (DP TASK 2)/subseq_template_length.cpp
@@ -6,27 +6,66 @@
 
 using namespace std;
 
+int getLCS(const string& x, const string& y)
+{
+    int xLen = x.length(), yLen = y.length();
+    int arr[x.length() + 1][y.length() + 1];
+
+    for (int i = 0; i <= yLen; ++i)
+        arr[0][i] = 0;
+
+    for (int i = 1; i <= xLen; ++i)
+    {
+        arr[i][0] = 0;
+        for (int j = 1; j <= yLen; ++j)
+        {
+            if (x[i - 1] == y[j - 1])
+                arr[i][j] = arr[i - 1][j - 1] + 1;
+            else
+                arr[i][j] = max(arr[i][j - 1], arr[i - 1][j]);
+        }
+    }
+
+    int i = xLen, j = yLen;
+    string out;
+    while (arr[i][j] != 0)
+    {
+        if (arr[i][j] == arr[i - 1][j])
+            --i;
+        else if (arr[i][j] == arr[i][j - 1])
+            --j;
+        else
+        {
+            out = x[i - 1] + out;
+            --i;
+            --j;
+        }
+    }
+
+    return out.size();
+}
+
 int main()
 {
-	string x;
-	string y;
-	int res;
+    string x;
+    string y;
+    int res;
 
-	ifstream fin;
-	fin.open("input.txt");
-	if (fin.is_open())
-	{
-		getline(fin, x);
-		getline(fin, y);
-		fin.close();
-	}
+    ifstream fin;
+    fin.open("input.txt");
+    if (fin.is_open())
+    {
+        getline(fin, x);
+        getline(fin, y);
+        fin.close();
+    }
 
-	res = getLCS(x, y);
+    res = getLCS(x, y);
 
-	fstream fout;
-	fout.open("output.txt", ios::out);
-	fout << res;
-	fout.close();
+    fstream fout;
+    fout.open("output.txt", ios::out);
+    fout << res;
+    fout.close();
 
-	return 0;
+    return 0;
 }
--- HW 12/Subseq search (DP TASK 3)/subseq_template_text.cpp
+++ HW 12/Subseq search (DP TASK 3)/subseq_template_text.cpp
@@ -8,31 +8,64 @@ using namespace std;
 
 string getLCS(string x, string y)
 {
-    return "";
+    int xLen = x.length(), yLen = y.length();
+    int arr[x.length() + 1][y.length() + 1];
+
+    for (int i = 0; i <= yLen; ++i)
+        arr[0][i] = 0;
+
+    for (int i = 1; i <= xLen; ++i)
+    {
+        arr[i][0] = 0;
+        for (int j = 1; j <= yLen; ++j)
+        {
+            if (x[i - 1] == y[j - 1])
+                arr[i][j] = arr[i - 1][j - 1] + 1;
+            else
+                arr[i][j] = max(arr[i][j - 1], arr[i - 1][j]);
+        }
+    }
+
+    int i = xLen, j = yLen;
+    string out;
+    while (arr[i][j] != 0)
+    {
+        if (arr[i][j] == arr[i - 1][j])
+            --i;
+        else if (arr[i][j] == arr[i][j - 1])
+            --j;
+        else
+        {
+            out = x[i - 1] + out;
+            --i;
+            --j;
+        }
+    }
+    return out;
 }
 
 int main()
 {
-	string x;
-	string y;
-	string res;
+    string x;
+    string y;
+    string res;
 
-	ifstream fin;
-	fin.open("input.txt");
-	if (fin.is_open())
-	{
-		getline(fin, x);
-		getline(fin, y);
-		fin.close();
-	}
+    ifstream fin;
+    fin.open("input.txt");
+    if (fin.is_open())
+    {
+        getline(fin, x);
+        getline(fin, y);
+        fin.close();
+    }
 
-	res = getLCS(x, y);
+    res = getLCS(x, y);
 
-	fstream fout;
-	fout.open("output.txt", ios::out);
-	fout << res;
-	fout.close();
+    fstream fout;
+    fout.open("output.txt", ios::out);
+    fout << res;
+    fout.close();
 
-	return 0;
+    return 0;
 }
 

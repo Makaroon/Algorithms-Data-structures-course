fd3cb408f02122c31abb5a41cb5724737ed2a9b2
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-05-04 21:41:30 +0300

    finished task 4

--- HW 12/Rocks (DP TASK 4)/main.cpp
+++ HW 12/Rocks (DP TASK 4)/main.cpp
@@ -7,15 +7,38 @@ using namespace std;
 
 //Задача реализовать этот метод
 //param N - количество камней
-//param M - ограничения на вес
+//param W - ограничения на вес
 //param stones - массив размера N, с весами камней
 //param res - вектор результатов (вес камней, которые надо взять)
 void solve(int N, int W, int* stones, vector<int>& res)
 {
-    
+    int a[N + 1][W + 1];
+
+    for (int i = 0; i < N + 1; ++i)
+        a[i][0] = 0;
+
+    for (int i = 0; i < W + 1; ++i)
+        a[0][i] = 0;
+
+    for (int i = 1; i < N + 1; ++i)
+        for (int j = 1; j < W + 1; ++j)
+        {
+            if (j - stones[i - 1] >= 0)
+                a[i][j] = max(a[i - 1][j], a[i - 1][j - stones[i - 1]] + stones[i - 1]);
+            else a[i][j] = a[i - 1][j];
+        }
+
+    for(int i = N, j = W; a[i][j] > 0; --i)
+    {
+        if(a[i][j] != a[i - 1][j])
+        {
+            res.emplace_back(stones[i - 1]);
+            j -= stones[i - 1];
+        }
+    }
 }
 
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
     ReadWriter rw;
     int N = rw.readInt(); //камни

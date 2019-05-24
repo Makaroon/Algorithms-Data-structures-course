13e8109eb6026c06615f15f1e56af684eea32ce5
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-05-24 14:42:58 +0300

    hw 14 task 1 done

--- HW 14/Combinatorics-1/main.cpp
+++ HW 14/Combinatorics-1/main.cpp
@@ -4,6 +4,7 @@
 //
 //  Created by Данил Нечай on 02.04.2018.
 //  Updated by Плесовских Александр on 07.09.2018.
+//  Finished by Ясиновский Александр on 24.05.2019.
 //  Copyright © 2018 Данил Нечай. All rights reserved.
 //
 
@@ -14,40 +15,56 @@
 #include <stdlib.h>
 #include <vector>
 #include <string>
+
 using namespace std;
 
 //Необходимо реализовать 3 метода (1,2,3).
 //Вся информация о задаче доступна в тексте задачи и в слайдах презентации к семинару(в ЛМС)
-static BigInteger FactTree(int n) //1
+static BigInteger FactTree(int n) //subtask 1
 {
-   
-}
-static BigInteger C(int n, int k) //2
-{
-    return ;
-}
-static BigInteger Pair (int n) //3
-{
-     return ;
+    BigInteger a(1);
+    for (int i = 1; i < n; ++i)
+        a *= (i + 1);
+    return a;
 }
 
-int main(int argc, const char * argv[]) {
+static BigInteger C(int n, int k)//subtask 2
+{
+    int maxN = max(n - k, k);
+    BigInteger a(1);
+    for (int i = maxN; i < n; ++i)
+        a *= (i + 1);
+    a /= FactTree(n - maxN);
+    return a;
+}
 
-    int n,N,K;
+static BigInteger Pair(int n)//subtask 3
+{
+    BigInteger a(1);
+    for (int i = 1; i < (n - 2); i += 2)
+        a *= (i + 2);
+    return a;
+}
+
+int main(int argc, const char* argv[])
+{
+
+    int n, N, K;
     fstream fin;
     fstream fout;
-    fin.open("input.txt",ios::in);
-    fout.open("output.txt",ios::out);
-    if(fin.is_open()) {
+    fin.open("input.txt", ios::in);
+    fout.open("output.txt", ios::out);
+    if (fin.is_open())
+    {
         string str;
-        getline(fin,str);
-        N = atoi( str.c_str());
+        getline(fin, str);
+        N = atoi(str.c_str());
         n = N;
-        getline(fin,str);
-        K = atoi( str.c_str());
-        fout << FactTree(N)<< endl;
-        fout << C(N,K)<< endl;
-        fout << Pair(N)<< endl;
+        getline(fin, str);
+        K = atoi(str.c_str());
+        fout << FactTree(N) << endl;
+        fout << C(N, K) << endl;
+        fout << Pair(N) << endl;
         fout.close();
         fin.close();
     }
